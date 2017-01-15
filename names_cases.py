f0ac17e26be8d398145578499d5f37f900fc2d8d
Igorth <igordiasth@gmail.com>
2017-01-15 19:17:04 -0200

    Exercises and Projects for the book Curso intensivo de python

---
+++ names_cases.py
@@ -0,0 +1,20 @@
+name = 'igor'
+print 'Alo ' + name + ' test'
+
+
+name = 'igor'
+print name.upper()
+print name.lower()
+print name.title()
+
+print 'Albert Einstein certa vez disse: "Uma pessoa que nunca cometeu um erro jamais tentou nada novo."'
+
+famous_person = 'Albert Einstein'
+message = '"Uma pessoa que nunca cometeu um erro jamais tentou nada novo"'
+print famous_person + ' certa vez disse: ' + message
+
+name = ' igor '
+print name
+print name.rstrip()
+print name.lstrip()
+print name.strip()
