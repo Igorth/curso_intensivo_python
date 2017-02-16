c7805fe70be7b792d89844a020a4d2f8481e93f8
Igorth <igordiasth@gmail.com>
2017-02-15 23:28:27 -0200

    Laço for em python

---
+++ for_magicians.py
@@ -0,0 +1,16 @@
+magicians = ['alice', 'igor', 'carolia']
+for item in magicians:
+    print item.title() + ", that was a great trick!"
+    print "I can't wait to see your next trick, " + item.title() + "\n"
+
+print "Thank you, everyone. That was a great magic show!" + "\n"
+
+pizzas = ['calabresa', 'peperoni', 'sugo']
+for pizza in pizzas:
+    print "Gosto de pizza de " + pizza
+print "I love pizza"
+
+animals = ['macacaco', 'elefante', 'tigre']
+for animal in animals:
+    print animal + " seria uma ótimo anima de estimação"
+print "I love animals"
