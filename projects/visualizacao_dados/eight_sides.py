6c2c64117e334f73f9a914a45b6e9dc68966e8f6
Igorth <igordiasth@gmail.com>
2017-03-20 23:13:08 -0300

    Aprendendo a utilizar a biblioteca pygal do python

--- projects/visualizacao_dados/eight_sides.py
+++ projects/visualizacao_dados/eight_sides.py
@@ -1,32 +1,34 @@
 import pygal
 from die import Die
 
-# Cria um D6
-die = Die()
+# Cria dois D8
+die_1 = Die(8)
+die_2 = Die(8)
 
 # Faz alguns lançamentos e armazena os resultados em uma lista
 results = []
 for roll_num in range(1000):
-	result = die.roll()
+	result = die_1.roll() + die_2.roll()
 	results.append(result)
 
 # Analisa os resultados
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(1, max_result+1):
 	frequency = results.count(value)
 	frequencies.append(frequency)
 
 # Visualiza os resultados
 hist = pygal.Bar()
 
-hist.title = "Results of rolling one D6 1000 times"
+hist.title = "Results of rolling two D8 dice 1000 times"
 hist.x_labels = []
-for item in range(1, 8):
+for item in range(2, 17):
 	hist.x_labels.append(item)
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6', frequencies)
-hist.render_to_file('die_visual.svg')
+hist.add('D8 + D8', frequencies)
+hist.render_to_file('eight_sides.svg')
 
 
