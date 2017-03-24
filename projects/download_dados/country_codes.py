048c86c02b18478428ea7e078b02436ce720916b
Igorth <igordiasth@gmail.com>
2017-03-23 21:54:39 -0300

    Grafico da população mundial utilizando json e pygal

---
+++ projects/download_dados/country_codes.py
@@ -0,0 +1,10 @@
+from pygal.maps.world import COUNTRIES
+
+def get_country_code(country_name):
+	"""Devolve o código de duas letras do Pygal para um pais, dando o seu nome."""
+	for code, name in COUNTRIES.items():
+		if name == country_name:
+			return code
+
+	# Se o país não foi encontrado, devolve None
+	return None
